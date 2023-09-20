77c188f7cf5f5f3542ebbded30d80fd351002f92
jamesnottidge <freshice57@gmail.com>
2023-09-20 18:45:57 +0100

    finished

--- mycliprogram.js
+++ mycliprogram.js
@@ -2,7 +2,8 @@ const args = process.argv;
 const commands = ["read", "write", "copy", "reverse"];
 const fs = require("fs");
 const readline = require("readline");
-
+const stream = require("stream");
+const Transform = stream.Transform || require("readable-stream").Transform;
 
 const getHelpText = function () {
   const helpText = `
@@ -41,18 +42,16 @@ if (args.length < 3) {
 
 switch (commands.indexOf(command)) {
   case 0:
-    console.log("command is read");
     read(args[3]);
     break;
   case 1:
-    console.log("command is write");
-
+    write(args[3]);
     break;
   case 2:
-    console.log("command is copy");
+    copy(args[3]);
     break;
   case 3:
-    console.log("command is reverse");
+    reverse(args[3])
     break;
   default:
     console.log(
@@ -62,15 +61,93 @@ switch (commands.indexOf(command)) {
     return;
 }
 
+function read(filePath) {
+  const readableStream = fs.createReadStream(filePath, { encoding: "utf-8" });
+  readableStream.on("data", (chunk) => {
+    console.log(chunk);
+  });
 
-function read (filePath) {
-    const readableStream = fs.createReadStream(filePath, { encoding: "utf-8" });
-    readableStream.on("data", (chunk) => {
-      console.log(chunk);
-    });
-
-    readableStream.on("error", (error) => {
-        console.error(error);
-    });
+  readableStream.on("error", (error) => {
+    console.error(error);
+  });
 }
 
+function write(filePath) {
+  const writableStream = fs.createWriteStream(filePath, { encoding: "utf-8" });
+  writableStream.on("error", (error) => {
+    console.error(error);
+  });
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Enter a sentence: ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", (line) => {
+    switch (line.trim()) {
+      case "exit":
+        rl.close();
+        break;
+      default:
+        sentence = line + "\n";
+        writableStream.write(sentence);
+        rl.prompt();
+        return;
+    }
+  }).on("close", () => {
+    writableStream.end();
+    writableStream.on("finish", () => {
+      console.log(`All your sentences have been written to ${filePath}`);
+    });
+    setTimeout(() => {
+      process.exit(0);
+    }, 100);
+  });
+}
+
+function copy(filePath) {
+  const inputStream = fs.createReadStream(filePath);
+  const fileCopyPath =
+    filePath.split(".")[0] + "copy." + filePath.split(".")[1];
+  const outputStream = fs.createWriteStream(fileCopyPath);
+
+  inputStream.pipe(outputStream);
+
+  outputStream.on("finish", () => {
+    console.log(`A copy of ${filePath} has been created at ${fileCopyPath}`);
+    outputStream.end();
+  });
+
+  inputStream.on("error", (error) => {
+    console.error(error);
+  });
+
+}
+
+
+function reverse(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const reversedDataFilePath =
+    filePath.split(".")[0] + "-reversed." + filePath.split(".")[1];
+  const writeStream = fs.createWriteStream(reversedDataFilePath);
+
+  const reverseStream = new Transform({
+    transform(data, encoding, callback) {
+      const reversedData = data.toString().split("").reverse().join("");
+      this.push(reversedData);
+      callback();
+    },
+  });
+
+  readStream
+    .pipe(reverseStream)
+    .pipe(writeStream)
+    .on("finish", () => {
+      console.log(
+        `Finished reversing the contents of ${filePath} and saving the output to ${reversedDataFilePath}.`
+      );
+    });
+}
